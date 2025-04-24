50988e646572be0f664949a87eb049c2bd5f1ef6
hoanglam2808 <hoanglam28082003@gmail.com>
2025-04-25 00:43:29 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,5 +1,5 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Tớ có điều quan trọng muốn nói. </br> Đồng ý làm người yêu tớ nhé, Phương Chi?",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
   buttonYes: "Tớ đồng ý",
