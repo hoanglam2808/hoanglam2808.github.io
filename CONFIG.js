e0890ae416f18d8c76b4a744eaf43c9a8d730870
hoanglam2808 <hoanglam28082003@gmail.com>
2025-04-24 23:12:56 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ muốn đánh cược toàn bộ tình yêu này với cậu. </br> Nếu tớ thắng thì tớ lấy cậu, nếu tớ thua thì cậu lấy tớ, còn nếu hòa thì chúng mình lấy nhau🥰.</br> Love you <3",
+    "Cảm ơn cậu đã cho tớ một cơ hội. </br> Tớ muốn đánh cược toàn bộ tình yêu này với cậu. Nếu tớ thắng thì tớ lấy cậu, nếu tớ thua thì cậu lấy tớ, còn nếu hòa thì chúng mình lấy nhau🥰.</br> Love you <3",
 };
